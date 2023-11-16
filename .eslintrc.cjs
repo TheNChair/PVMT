3a248d944466c07cb5d495e20462a3b7434b17de
neillin <Neil_YC_Lin@wistron.com>
2023-11-16 09:44:50 +0800

    [feature] install react prettier eslint and husky

---
+++ .eslintrc.cjs
@@ -0,0 +1,56 @@
+module.exports = {
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+        'prettier',
+        'plugin:prettier/recommended',
+        'plugin:import/recommended'
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    plugins: ['@typescript-eslint', 'react', 'react-hooks', 'react-refresh'],
+    rules: {
+        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+        indent: ['error', 4, { SwitchCase: 1 }],
+        quotes: ['error', 'single'],
+        semi: ['error', 'always'],
+        'semi-spacing': ['error'],
+        'comma-dangle': ['error', 'never'],
+        'comma-spacing': 'error',
+        'key-spacing': 'error',
+        'arrow-spacing': 'error',
+        'space-infix-ops': 'error',
+        'brace-style': ['error', '1tbs', { allowSingleLine: true }],
+        camelcase: 'error',
+        'new-cap': 'error',
+        'space-before-blocks': 'error',
+        'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+        'no-var': 'error',
+        'no-unreachable': 'error',
+        'computed-property-spacing': ['error', 'never'],
+        curly: ['error', 'all'],
+        'no-unneeded-ternary': 'error',
+        'no-trailing-spaces': 'error',
+        'no-empty': 'error',
+        'react/prop-types': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-uses-react': 'off'
+    },
+    settings: {
+        'import/resolver': {
+            node: {
+                extensions: ['.js', '.jsx', '.ts', '.tsx']
+            }
+        }
+    }
+};
