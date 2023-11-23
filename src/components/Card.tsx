11c1bb14097ac6fafaf3043ac0bc5c7cf7658862
neillin <Neil_YC_Lin@wistron.com>
2023-11-23 20:08:48 +0800

    [modfiy] modify Card style

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -27,44 +27,74 @@ const CandidateRow = styled.div<{ isElected?: boolean }>`
 `;
 
 const CandidateNameWrapper = styled.div`
-    flex: 0.3;
+    flex-basis: 30%;
     display: flex;
     align-items: center;
     column-gap: 1rem;
+    position: relative;
 `;
 
 const CandidateName = styled.span`
     font-family: 'Noto Sans';
+    margin-left: 1.5rem;
 `;
 
 const Proportion = styled.span`
-    flex: 0.2;
+    flex-basis: 20%;
     text-align: left;
 `;
 
 const Amount = styled.span`
-    flex: 0.5;
+    flex-basis: 50%;
     text-align: left;
 `;
 
+const IndicatorWrapper = styled.span<{ color: string; elected: boolean }>`
+    position: absolute;
+    & svg {
+        margin-left: ${({ elected }) => (elected ? '-0.2rem' : 0)};
+        filter: ${({ color }) => (color ? `drop-shadow(2px 2px 8px ${color})` : undefined)};
+        width: ${({ elected }) => (elected ? '20px' : '12px')};
+        height: ${({ elected }) => (elected ? '20px' : '12px')};
+    }
+`;
+
+const CANDIDATE_MAP = new Map([
+    [
+        1,
+        {
+            element: <DDPNormal />,
+            color: '#03ff57'
+        }
+    ],
+    [
+        2,
+        {
+            element: <KMTNormal />,
+            color: '#00e0ff'
+        }
+    ],
+    [
+        2,
+        {
+            element: <PFPNormal />,
+            color: '#fff500'
+        }
+    ]
+]);
+
 const Card = () => {
     // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
-    const [isElected, setIsElected] = useState(true);
+    const [elected, setElected] = useState(1);
 
     return (
         <Wrapper>
             <Title>區域名稱</Title>
             <CandidateRow>
                 <CandidateNameWrapper>
-                    <DDPNormal
-                        style={
-                            isElected
-                                ? { filter: 'drop-shadow(2px 2px 8px #00e0ff)', marginLeft: '-0.2rem' }
-                                : undefined
-                        }
-                        width={isElected ? 20 : 12}
-                        height={isElected ? 20 : 12}
-                    />
+                    <IndicatorWrapper color={CANDIDATE_MAP.get(elected)?.color} elected>
+                        {CANDIDATE_MAP.get(elected)?.element}
+                    </IndicatorWrapper>
                     <CandidateName>蔡英文</CandidateName>
                 </CandidateNameWrapper>
                 <Amount>8,170,231票</Amount>
@@ -72,7 +102,7 @@ const Card = () => {
             </CandidateRow>
             <CandidateRow>
                 <CandidateNameWrapper>
-                    <KMTNormal />
+                    <KMTNormal style={{ position: 'absolute' }} />
                     <CandidateName>韓狗魚</CandidateName>
                 </CandidateNameWrapper>
                 <Amount>5,170,231票</Amount>
@@ -80,7 +110,7 @@ const Card = () => {
             </CandidateRow>
             <CandidateRow>
                 <CandidateNameWrapper>
-                    <PFPNormal />
+                    <PFPNormal style={{ position: 'absolute' }} />
                     <CandidateName>宋楚魚</CandidateName>
                 </CandidateNameWrapper>
                 <Amount>170,231票</Amount>
