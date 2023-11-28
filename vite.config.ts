d638fca6e4ca6afafbe0b8d587f64a23ed9cf297
Ben Yu <Ben_BZ_Yu@wistron.com>
2023-11-29 01:55:55 +0800

    [modify] adjust config

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,6 @@ export default defineConfig({
             '@': path.resolve(__dirname, 'src')
         },
         extensions: ['.js', '.jsx', '.ts', '.tsx']
-    }
+    },
+    base: '/PVMT/'
 });
