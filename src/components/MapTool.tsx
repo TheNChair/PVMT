cb2215e0d357e12f5c81af41902dac8f25f0fac5
neillin <Neil_YC_Lin@wistron.com>
2023-11-29 10:51:30 +0800

    [feature] change hover area color

--- src/components/MapTool.tsx
+++ src/components/MapTool.tsx
@@ -4,19 +4,19 @@ export const IndicatorWrapper = styled.span<{ $isMobile?: boolean }>`
     position: absolute;
     right: 0;
     top: ${({ $isMobile }) => ($isMobile ? undefined : '5%')};
-    bottom: ${({ $isMobile }) => ($isMobile ? '25%' : undefined)};
+    bottom: ${({ $isMobile }) => ($isMobile ? '28%' : undefined)};
 `;
 
 export const CircleButton = styled.span`
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 30px;
-    height: 30px;
+    width: 50px;
+    height: 50px;
     margin: 1rem 0.5rem;
     border-radius: 50%;
-    border: 0.5px solid;
+    border: 1px solid;
     border-image-source: linear-gradient(135deg, #ffffff 12.5%, rgba(255, 255, 255, 0) 75%);
-    box-shadow: 0px 0px 4px 0px rgba(184, 184, 184, 0.25);
+    box-shadow: 0px 0px 4px 0px rgba(239, 239, 239, 0.25);
     cursor: pointer;
 `;
